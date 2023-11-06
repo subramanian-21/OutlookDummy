59737a1584408613ba1d05fd30987051c29c334b
thugperfect <meenamani9444@gmail.com>
2023-11-06 11:33:06 +0530

    adding deleted emails to []

--- script.js
+++ script.js
@@ -1,62 +1,79 @@
-const emails = [
+let emails = [
   {
     id:1,
     from: "beetlejuice@gmail.com",
     msg: "Hi Subramanian,Here are some of the latest opportunities matching your preferences. Apply now!",
-    fav:false
+    fav:false,
+    deleted:false
   },
   {
     id:2,
     from: "richardtheretard@gmail.com",
     msg: "College students can taste placement success with this course!",
-    fav:false
+    fav:false,
+    deleted:false
   },
   {
     id:3,
     from: "dumbass@email.com",
     msg: "“software engineer”: ADSMN - HTML CSS JS - Frontend Developer and more",
-    fav:false
+    fav:false,
+    deleted:false
+
   },
   {
     id:4,
     from: "noone@gmail.com",
     msg: "Invitation to participate in the first pilot of Central Bank Digital Currency - Retail (Digital Rupee - e₹) / SBI CBDC",
-    fav:false
+    fav:false,
+    deleted:false
+
   },
   {
     id:5,
     from: "bb12372@gmail.com",
     msg: "Reminder: Verify your email to continue to Proton",
-    fav:false
+    fav:false,
+    deleted:false
+
   },
   {
     id:6,
     from: "wakali@gmail.com",
     msg: "Get Tech Placed | Join the 20X Programming Bundle",
-    fav:false
+    fav:false,
+    deleted:false
+
   },
   {
     id:7,
     from: "captainalex@email.com",
     msg: "Subramanian, new internships in DataSkience OPC, Hooman Digital LLP & more",
-    fav:false
+    fav:false,
+    deleted:false
+
   },
   {
     id:8,
     from: "jamaalbee@rediff.com",
     msg: "Subramanian, new internships in DataSkience OPC, Hooman Digital LLP & more",
-    fav:false
+    fav:false,
+    deleted:false
+
   },
   {
     id:9,
     from: "niggatron@gmail.com",
     msg: "Wanna know how 'typography' affects the human mind? 🤯 Register to know more!",
-    fav:false
+    fav:false,
+    deleted:false
+
   },
 ];
 const clickedEmail = {};
 const visitedEmails = [];
-let favouriteEmails =[]
+let favouriteEmails =[];
+let deleteEmails = []
 let navBtns = "";
 let selectedEmail;
 let val = null
@@ -107,8 +124,12 @@ function inbox() {
     const emailFrom = document.createElement("div");
     const emailBody = document.createElement("div");
     const emailFav = document.createElement("i")
-    const msg = (k.msg.length>60)?k.msg.slice(0,60)+"...":k.msg
+    const deleteEmail = document.createElement("i")
+    const msg = (k.msg.length>40)?k.msg.slice(0,40)+"...":k.msg
+    deleteEmail.id = "delete-email"
     emailFav.id = "email-fav"
+    deleteEmail.classList.add("bi")
+    deleteEmail.classList.add("bi-trash-fill")
     emailFav.classList.add("bi")
     emailFav.classList.add("bi-star-fill")
       emailBox.classList.add("email-box");
@@ -119,11 +140,21 @@ function inbox() {
       emailBox.appendChild(emailFav)
       emailBox.appendChild(emailFrom);
       emailBox.appendChild(emailBody);
+      emailBox.appendChild(deleteEmail)
       left.appendChild(emailBox);
       emailBox.addEventListener("click", () => rightfn(k, emailBox));
       emailFav.addEventListener("click",()=>addToFav(k,emailFav))
+      deleteEmail.addEventListener("click",()=>deletedEmail(k,emailBox))
       
   });
+}
+function deletedEmail(k,e){
+    e.style.display = "none"
+    k.deleted = true
+    deleteEmails = emails.filter(k=>{
+        return k.deleted
+    })
+    console.log(deleteEmails)
 }
 
 function addToFav(k,e){
